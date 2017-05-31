918ec0e5f7cf434160d671c8c48a81e08ac27834
Gentle_Asus <hit_zhantaodeng@126.com>
2017-05-31 22:02:09 +0800

    65% of KeyFrame.h

--- include/KeyFrame.h
+++ include/KeyFrame.h
@@ -43,7 +43,7 @@ class KeyFrameDatabase;
 class KeyFrame
 {
 public:
-    KeyFrame(Frame &F, Map* pMap, KeyFrameDatabase* pKFDB);
+    KeyFrame(Frame &F, Map* pMap, KeyFrameDatabase* pKFDB);     // 设置实例中的标志和采纳数，将Frame中的Grid复制一份(vector<vector<vector>>)
 
     // Pose functions
     void SetPose(const cv::Mat &Tcw);
@@ -54,18 +54,18 @@ public:
     cv::Mat GetRotation();
     cv::Mat GetTranslation();
 
-    // Bag of Words Representation
+    // 计算本实例的BoW, 保存在BoWVec 和FeatureVec（FeatureVec选择在第4层，一种可能是为了提高搜索效率，层数越多指数增加）
     void ComputeBoW();
 
     // Covisibility graph functions
-    void AddConnection(KeyFrame* pKF, const int &weight);
+    void AddConnection(KeyFrame* pKF, const int &weight);       // 为本实例与指定KeyFrame之间添加连接与权值（ map<KeyFrame*, in> ）,用于建立共视关键帧间的联系
     void EraseConnection(KeyFrame* pKF);
-    void UpdateConnections();
-    void UpdateBestCovisibles();
-    std::set<KeyFrame *> GetConnectedKeyFrames();
+    void UpdateConnections();       // 更新本实例的KF连接，形成convisibility graph
+    void UpdateBestCovisibles();        // 在Add与Erase函数中被调用，对连接到本实例的共视KF按连接权值进行排序
+    std::set<KeyFrame *> GetConnectedKeyFrames();       // 返回与本实例连接的所有KF的指针
     std::vector<KeyFrame* > GetVectorCovisibleKeyFrames();
-    std::vector<KeyFrame*> GetBestCovisibilityKeyFrames(const int &N);
-    std::vector<KeyFrame*> GetCovisiblesByWeight(const int &w);
+    std::vector<KeyFrame*> GetBestCovisibilityKeyFrames(const int &N);  // 返回按权重排序前N个的共视关键帧
+    std::vector<KeyFrame*> GetCovisiblesByWeight(const int &w);     // 返回权重在 w 以上的共视关键帧
     int GetWeight(KeyFrame* pKF);
 
     // Spanning tree functions
@@ -86,13 +86,13 @@ public:
     void EraseMapPointMatch(MapPoint* pMP);
     void ReplaceMapPointMatch(const size_t &idx, MapPoint* pMP);
     std::set<MapPoint*> GetMapPoints();
-    std::vector<MapPoint*> GetMapPointMatches();
-    int TrackedMapPoints(const int &minObs);
-    MapPoint* GetMapPoint(const size_t &idx);
+    std::vector<MapPoint*> GetMapPointMatches();    // 获取与本实例中keypoint关联的MapPoints
+    int TrackedMapPoints(const int &minObs);        // 返回本实例中高质量的MapPoints数量; 高质量指被多于指定的minObs个关键帧看到
+    MapPoint* GetMapPoint(const size_t &idx);       // 与上第二行相似，但会滤除不良的点以及没有match的点
 
     // KeyPoint functions
-    std::vector<size_t> GetFeaturesInArea(const float &x, const float  &y, const float  &r) const;
-    cv::Mat UnprojectStereo(int i);
+    std::vector<size_t> GetFeaturesInArea(const float &x, const float  &y, const float  &r) const;      // 获取指定位置区域的keypoints
+    cv::Mat UnprojectStereo(int i);     // 将指定的第i个Keypoints反投影到3D中
 
     // Image
     bool IsInImage(const float &x, const float &y) const;
@@ -122,7 +122,7 @@ public:
 
     static long unsigned int nNextId;
     long unsigned int mnId;
-    const long unsigned int mnFrameId;
+    const long unsigned int mnFrameId;      // 记录本实例由哪一个Frame初始化
 
     const double mTimeStamp;
 
@@ -133,18 +133,18 @@ public:
     const float mfGridElementHeightInv;
 
     // Variables used by the tracking
-    long unsigned int mnTrackReferenceForFrame;
-    long unsigned int mnFuseTargetForKF;
+    long unsigned int mnTrackReferenceForFrame;     // 在Tracking中使用，如果本实例被加入到某一CurrentFrame 的LocalKeyFrame，则本变量记录CurrentFrameID
+    long unsigned int mnFuseTargetForKF;        // 标记本实例在LocalMapping的融合MapPoints步骤已经被使用
 
-    // Variables used by the local mapping
-    long unsigned int mnBALocalForKF;
-    long unsigned int mnBAFixedForKF;
+    // Variables used by the local mapping， 取值为零或者本实例的mnId时分别表示在LocalBA中是否参与优化并被修正
+    long unsigned int mnBALocalForKF;   // 非零则本实例参加优化？
+    long unsigned int mnBAFixedForKF;   // 非零则本实例不参加优化？
 
     // Variables used by the keyframe database
-    long unsigned int mnLoopQuery;
-    int mnLoopWords;
-    float mLoopScore;
-    long unsigned int mnRelocQuery;
+    long unsigned int mnLoopQuery;      // 将本实例标记为Loop候选帧的 正在进行LoopClosing的帧的编号
+    int mnLoopWords;        // 记录本实例与其他某一KF共同的words个数，DetectLoopCandidates（）
+    float mLoopScore;       // 记录本实例在DetectLoopCandidate()中的得分
+    long unsigned int mnRelocQuery; // 以下三变量与上述三个变量含义相似
     int mnRelocWords;
     float mRelocScore;
 
@@ -170,7 +170,7 @@ public:
     DBoW2::BowVector mBowVec;
     DBoW2::FeatureVector mFeatVec;
 
-    // Pose relative to parent (this is computed when bad flag is activated)
+    // 相对于亲节点的位姿，badflag为真时才计算
     cv::Mat mTcp;
 
     // Scale
@@ -203,18 +203,18 @@ protected:
     std::vector<MapPoint*> mvpMapPoints;
 
     // BoW
-    KeyFrameDatabase* mpKeyFrameDB;
-    ORBVocabulary* mpORBvocabulary;
+    KeyFrameDatabase* mpKeyFrameDB;     // 指向KeyFrameDataBase实例的指针，KFDB包含所有KF,检测Loop,Reloc以及词典
+    ORBVocabulary* mpORBvocabulary;     // ORB词汇表指针，与上一类中包含的词典相同
 
     // Grid over the image to speed up feature matching
     std::vector< std::vector <std::vector<size_t> > > mGrid;
 
-    std::map<KeyFrame*,int> mConnectedKeyFrameWeights;
-    std::vector<KeyFrame*> mvpOrderedConnectedKeyFrames;
+    std::map<KeyFrame*,int> mConnectedKeyFrameWeights;      // 与本实例连接的KF以及权重
+    std::vector<KeyFrame*> mvpOrderedConnectedKeyFrames;        // 按权重排序的KF序列
     std::vector<int> mvOrderedWeights;
 
     // Spanning Tree and Loop Edges
-    bool mbFirstConnection;
+    bool mbFirstConnection;     // 更新生成树时使用，完成双向连接后置为false
     KeyFrame* mpParent;
     std::set<KeyFrame*> mspChildrens;
     std::set<KeyFrame*> mspLoopEdges;
